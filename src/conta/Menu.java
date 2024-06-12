04ab707b7d045d35cbeb12dcfb02c12e6a33f9f9
Victor Oliveira <victor.oliveira20@icloud.com>
2024-06-12 13:55:36 -0300

    Classe ModelConta criada, testes realizados

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -2,13 +2,22 @@ package conta;
 
 import java.util.Scanner;
 
+import conta.model.ModelConta;
 import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
-		
+						
 		Scanner sc = new Scanner(System.in);
+		
+		ModelConta c1 = new ModelConta(1, 123, 1, "Adriana", 10000.0f);
+		
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
 
 		int opcao;
 
